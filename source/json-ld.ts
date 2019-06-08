dbb9f48a8f6e278a59d1070beeb57440da119bc5
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-08 15:02:55 +0200

    vamtiger-node-typescript-commit

--- source/json-ld.ts
+++ source/json-ld.ts
@@ -1,23 +1,12 @@
 const jsonLd = {
     '@context': 'http://schema.org',
-    '@type': 'Logo',
+    '@type': 'Brand',
     name: 'Arting Health',
-    description: 'Arting Health Logo',
-    datePublished: '05 June 2019',
-    image: 'https://unpkg.com/vamtiger-arting-health-logo-json-ld@latest/vamtiger-mental-health-botswana-2018-json-ld.jpg',
-    creator: [
-        {
-            '@type': 'Organization',
-            name: 'Infers Group',
-            url: 'http://artinghealth.infersgroup.com',
-            email: 'info(at)infersgroup.com'
-        }
-    ],
-    funder: {
-        '@type': 'Organization',
-        name: 'Welcome',
-        url: 'https://wellcome.ac.uk'
-    }
+    description: 'Arting Health is a multilateral and multinational collaborative project aimed at engaging and informing communities on the dynamics of socially relevant issues in health and disease. With medical science as a source of inspiration and art as a medium for engagement, we innovate and implement novel strategies for effectively communicating the art of science to the public. The project strives to promote the importance of funding scientists at the forefront of public health and biomedical research, and encourage the support of artists who push the boundaries of creativity, with the ultimate goal of highlighting the impact of health related problems plaguing communities in a manner which positively influences policy.',
+    slogan: 'Science Communication and Public Engagement',
+    alternateName: 'Arting Health for Impact',
+    image: 'https://unpkg.com/vamtiger-arting-health-logo-json-ld@latest/arting-health-for-impact-icon.html',
+    url: 'https://artinghealth.infersgroup.com'
 };
 
 export default [
