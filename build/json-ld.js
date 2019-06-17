d248edb44aa5c12915bad102555ca95d78a586a6
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-17 17:55:06 +0200

    test

--- build/json-ld.js
+++ build/json-ld.js
@@ -7,7 +7,7 @@ var jsonLd = {
     description: 'Arting Health is a multilateral and multinational collaborative project aimed at engaging and informing communities on the dynamics of socially relevant issues in health and disease. With medical science as a source of inspiration and art as a medium for engagement, we innovate and implement novel strategies for effectively communicating the art of science to the public. The project strives to promote the importance of funding scientists at the forefront of public health and biomedical research, and encourage the support of artists who push the boundaries of creativity, with the ultimate goal of highlighting the impact of health related problems plaguing communities in a manner which positively influences policy.',
     slogan: 'Science Communication and Public Engagement',
     alternateName: 'Arting Health for Impact',
-    image: 'https://vamtiger-project.github.io/vamtiger-arting-health-logo-json-ld/build/arting-health-for-impact-icon.html',
+    image: 'https://vamtiger-project.github.io/vamtiger-arting-health-logo-json-ld/arting-health-for-impact-icon.html',
     url: 'https://artinghealth.infersgroup.com'
 };
 exports.default = [
